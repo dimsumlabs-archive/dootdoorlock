4a1512c968af04739dcf33ce74d9b03a05503dd2
Mathis Antony <sveitser@gmail.com>
2013-02-12 19:29:49 +0800

    working. sort of.

--- door.py
+++ door.py
@@ -1,59 +1,64 @@
 #!/usr/bin/python
 #
-# untested and WIP
+# TODO: easily add users
+# 
+# To run: ./door.py
+#
+# Add users:
+# start python shell and execute:
+# 	import door
+# 	users, keys = door.read_users("users.txt")
+# 	door.add_user(users, keys, "Lastname  Firstname", door.read_user())
+# then go beep your card
+# restart ./door.py
 #
 from __future__ import print_function
-import os, subprocess
-from hashlib import sha512 as hashfun
+import subprocess, serial, re
+from hashlib import sha256 as hashfun
 from time import sleep
-b = 'A0B0C0D0E0'
-found = 0
 
-octopus_reader ="/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_\
-        Controller_0001-if00-port0"
-doorlock = "/dev/serial/by-id/usb-usb-Prolific_Technology_Inc._USB-Serial_\
-        Controller-if00-port0"
+octopus_reader ="/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0"
+doorlock = "/dev/serial/by-id/usb-Prolific_Technology_Inc._USB-Serial_Controller-if00-port0"
+ser = serial.Serial(octopus_reader, 9600)
+userdata = "users.txt"
 
 def read_users(fname):
     users, keys = dict(), dict()
     with open(fname) as userfile:
         for line in userfile:
-	    name, key = line.rsplit(" ", 1)
+	    name, key = line.strip("\n").rsplit(" ", 1)
             users[name] = key
             keys[key] = name
     return users, keys
 
-def save_users(fname, users):
-    with open(fname, 'w') as userfile:
+def save_users(users):
+    with open(userdata, 'w') as userfile:
         for name in sorted(users):
             print("{0} {1}".format(name, users[name]), file=userfile)
+    print("Saved users")
 
 def add_user(users, keys, name, key):
     if name in users:
         print("{0} is already in database. To overwrite type: y".format(name))
-        if raw_input().lower() is not "y":
+        if raw_input().lower() != "y":
+	    print("not adding")
             return
     users[name] = key
     keys[key] = name
+    save_users(users)
 
 def open_door():
-    p = subprocess.Popen("cat {0}".format(doorlock))
+    p = subprocess.Popen(["cat", doorlock])
     sleep(5)
     p.terminate()
 
+def read_user():
+    return hashfun(re.sub("[^0-F]", "", ser.readline())).hexdigest()
+
 if __name__ == "__main__":
     users, keys = read_users("users.txt")
-    print(users,keys)
-    #while True:
-    #	print("blah")
-        #with open(octopus_reader) as reader:
-        #    for line in reader:
-        #        if hashfun(line) in keys
-        #for line in users:
-        #    if b in line:
-        #        print("FOUND")
-        #        found = 1
-        #users.close()
-        #if found == 0:
-        #    print("NOT FOUND.")
-        #reader.close()
+    while True:
+	key = read_user()
+	if key in keys:
+            print("Opening for {0}".format(keys[key]))
+	    open_door()
