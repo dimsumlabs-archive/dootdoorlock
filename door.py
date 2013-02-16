5a41a1fbff065e46df9f8f79e09f68bd614c5da3
Mathis Antony <sveitser@gmail.com>
2013-02-16 18:22:00 +0800

    Daemon working

--- door.py
+++ door.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python
 #
-# TODO: run as daemon
-#       maybe with http://pypi.python.org/pypi/python-daemon/
-# 
 # usage:
 #   to run: ./door.py
 #   to add users: ./door.py add
@@ -16,11 +13,10 @@ octopus_reader ="/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Co
 doorlock = "/dev/serial/by-id/usb-Prolific_Technology_Inc._USB-Serial_Controller-if00-port0"
 ser = serial.Serial(octopus_reader, 9600)
 userdata = "users.txt"
-pidfile = "/tmp/dootdoorlock.pid"
 
-def read_users(fname):
+def read_users():
     users = dict()
-    with open(fname) as userfile:
+    with open(userdata) as userfile:
         for line in userfile:
 	    name, key = line.strip("\n").rsplit(" ", 1)
             users[name] = key
@@ -33,7 +29,7 @@ def save_users(users):
     print("Saved users")
 
 def add_user():
-    users = read_users(fname)
+    users = read_users()
     print("Enter name (lastname firstname) of user to add then press enter.")
     name = raw_input()
     print("Now scan octopus card.")
@@ -62,27 +58,8 @@ def run():
             print("Opening for {0}".format(keys[key]))
             open_door()
 
-def main(argv):
-    if len(argv) == 3:
-        if argv[2] == "add":
-            print("To add user press y followed by enter.")
-            while raw_input() == "y":
-                add_user()
-                print("To add another user press y followed by enter.")
-        else if argv[2] == "run":
-            run()
-
-
-    if os.path.isfile(pidfile):
-        print("Doorlock already running, I'm killing it.")
-        old_pid = int(open(pidfile).read())
-        os.kill(pid, signal.SIGKILL)
-    
-    # nohup prevents the process from dying with the parent shell
-    # couldn't test this yet!
-    p = subprocess.Popen(["nohup", "python", "door.py", "run"])
-    print(p.pid, file=open(pidfile, 'w'))
-    print("Running door.py in background.")
-
 if __name__ == "__main__":
-    main(sys.argv)
+    if sys.argv[1] == "add":
+	add_user()
+    else:
+	run()
