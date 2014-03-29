f9854be033874236ea3cf9f01ea9908d47e916f3
Doorlock Raspberry Pi <hello@dimsumlabs.com>
2014-03-29 10:01:05 +0000

    [davidleung] Commiting Tom's twitter code

--- door.py
+++ door.py
@@ -6,11 +6,22 @@
 #
 from __future__ import print_function
 import subprocess, serial, sys, os
+import time
 from time import sleep
 from octopus import Octopus
+from twython import Twython
 
 unlock = "1 > /sys/class/gpio/gpio17/value"
 lock = "0 > /sys/class/gpio/gpio17/value"
+
+CONSUMER_KEY ='2DKmty7tJrdqQEp6K6rbBQ'
+CONSUMER_SECRET = 'hTk77aiSsvLaeAwQctWOLV4XOcy138sjRDMxTyo'
+ACCESS_KEY = '2248113774-oEbT4i32DwP8qw7Xlu2t3bRWvN1XaPn7XQqUxdP'
+ACCESS_SECRET = 'qJlgkMGTrOz46AvwuGzeTz8JMB7dT7BsCl7A85A662EJE'
+
+api = Twython(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_KEY,ACCESS_SECRET)
+os.environ['TZ'] = 'GMT+16'
+time.tzset()
 
 class Door:
     def __init__(self, seconds=5):
@@ -24,7 +35,7 @@ class Door:
 	os.popen("echo 1 > /sys/class/gpio/gpio17/value")
 	sleep(self.seconds)
         os.popen("echo 0 > /sys/class/gpio/gpio17/value")
-	
+	api.update_status(status='Someone entered DSL at '+time.strftime('%H:%M:%S'))
 
     def run(self, auth_module):
         while True:
@@ -37,7 +48,7 @@ class Door:
 
 if __name__ == "__main__":
     door = Door()
-    octopus = Octopus("users.txt")
+    octopus = Octopus("/home/pi/door/dootdoorlock/users.txt")
     print ("running")	
     if len(sys.argv) == 2: 
         if sys.argv[1] == "add":
